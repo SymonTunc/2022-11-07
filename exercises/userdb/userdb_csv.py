1ee4e19348815b0b329e5bf931b79e698aee63b1
Joerg Faschingbauer <jf@faschingbauer.co.at>
2022-11-08 12:58:54 +0100

    new requirements

--- exercises/userdb/userdb_csv.py
+++ exercises/userdb/userdb_csv.py
@@ -15,3 +15,25 @@ def read_csv_noheader(filename):
         users.append(ud)
 
     return users
+
+def read_csv_header(filename):
+    users = []
+
+    f = open(filename, encoding='cp1252')
+    rdr = csv.DictReader(f, delimiter=';', quotechar='"')
+    for ud in rdr:
+        id = ud['ID']
+        firstname = ud['First name']
+        lastname = ud['Last name']
+        birth = ud['Date of Birth']
+
+        my_ud = {
+            'id': int(id),
+            'firstname': firstname,
+            'lastname': lastname,
+            'birth': birth,
+        }
+        users.append(my_ud)
+
+    return users
+    
